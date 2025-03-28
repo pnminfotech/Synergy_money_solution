c5f6bab524fbe0d62465b9dd03344bac33b68a7b
pnminfotech <pnminfotech2024@gmail.com>
2025-03-28 11:18:51 +0530

    first commit

--- routes/SipBRoutes.js
+++ routes/SipBRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const XLSX = require("xlsx");
-const Sip = require("../models/SipModel"); // Your Mongoose Model
+const Sip = require("../models/sipModel"); // Your Mongoose Model
 
 
 
